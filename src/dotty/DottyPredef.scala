b8cc4a32c006a5648a8d64e22e6cfd27b5b63ae6
Guillaume Martres <smarter@ubuntu.com>
2015-05-26 00:16:16 +0200

    New representation for arrays of value classes
    For a value class V whose underlying type is U, instead of representing
    an array of V as V[] on the JVM, we use our own class VCXArray where X
    is "U" if U is a primitive type and is "Object" otherwise.
    
    This avoids boxing when creating arrays but we still need to box and
    unbox when using such an array in a generic position, this also breaks
    reflection and makes it pretty hard to use an array of a value class
    from Java or Scala 2.

--- src/dotty/DottyPredef.scala
+++ src/dotty/DottyPredef.scala
@@ -10,4 +10,15 @@ object DottyPredef {
 
   implicit def arrayTag[T](implicit ctag: ClassTag[T]): ClassTag[Array[T]] =
     ctag.wrap
+
+  // This implicit will never be used for arrays of primitives because
+  // the wrap*Array in scala.Predef have a higher priority.
+  implicit def wrapVCArray[T <: AnyVal](xs: Array[T]): WrappedArray[T] =
+    new WrappedArray[T] {
+      val array = xs
+      lazy val elemTag = ClassTag[T](arrayElementClass(array.getClass))
+      def length: Int = array.length
+      def apply(index: Int): T = array(index)
+      def update(index: Int, elem: T) { array(index) = elem }
+    }
 }
