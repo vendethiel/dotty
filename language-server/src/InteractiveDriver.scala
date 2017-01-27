cc624244a765b68ad71c40e80a5b2e515c67ae0c
Guillaume Martres <smarter@ubuntu.com>
2017-01-27 18:24:42 +0100

    Search for tasty classes recursively

--- language-server/src/InteractiveDriver.scala
+++ language-server/src/InteractiveDriver.scala
@@ -101,7 +101,11 @@ class ServerDriver(settings: List[String]) extends Driver {
 
   def trees = {
     val sourceClasses = openClasses.values.flatten
-    val tastyClasses = ctx.platform.classPath.classes.map(_.name.toTypeName)
+
+    def classPathClasses(cp: ClassPath): IndexedSeq[ClassPath#ClassRep] =
+      cp.classes ++ cp.packages.flatMap(classPathClasses)
+
+    val tastyClasses = classPathClasses(ctx.platform.classPath).map(_.name.toTypeName)
 
     (sourceClasses.flatMap(c => tree(c, fromSource = true)) ++
       tastyClasses.flatMap(c => tree(c, fromSource = false))).toList
