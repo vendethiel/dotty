090d05d207e5e1146244becda42ee4f613144908
Guillaume Martres <smarter@ubuntu.com>
2017-04-22 23:48:37 +0200

    Support -port, -stdio

--- language-server/src/Main.scala
+++ language-server/src/Main.scala
@@ -4,7 +4,7 @@ package interactive
 
 import java.util.function.Consumer
 
-import java.io._
+import java.io.{ InputStream, OutputStream }
 import java.net._
 
 import org.eclipse.lsp4j._
@@ -13,60 +13,37 @@ import org.eclipse.lsp4j.launch._
 
 object Main {
   def main(args: Array[String]): Unit = {
+    val portOpt = args.dropWhile(_ != "-port").drop(1).headOption.map(_.toInt)
 
-    val serverSocket = new ServerSocket(6004)
-    Runtime.getRuntime().addShutdownHook(new Thread(
-      new Runnable {
-        def run: Unit = {
-          serverSocket.close()
-        }
-      }));
+    val (in: InputStream, out: OutputStream) = args.toList match {
+      case List("-port", port) =>
+        val serverSocket = new ServerSocket(port.toInt)
+        Runtime.getRuntime().addShutdownHook(new Thread(
+          new Runnable {
+            def run: Unit = {
+              serverSocket.close()
+            }
+          }));
+
+        val clientSocket = serverSocket.accept()
+        (clientSocket.getInputStream, clientSocket.getOutputStream)
+      case List("-stdio") =>
+        (System.in, System.out)
+      case _ =>
+        Console.err.println("Invalid arguments: expected \"-stdio\" or \"-port NNNN\"")
+        System.exit(1)
+    }
 
     val server = new ScalaLanguageServer
 
-    println("hi")
-    val clientSocket = serverSocket.accept()
-    println("accepted")
-    val in = clientSocket.getInputStream
-    val out = clientSocket.getOutputStream
-    //val in = System.in
-    //val out = System.out
-
-    // val launcher = LSPLauncher.createServerLauncher(server, in, out, false, new PrintWriter(System.err, true))
-    val launcher = LSPLauncher.createServerLauncher(server, in, out)
-    val client = launcher.getRemoteProxy()
-    server.connect(client)
-    launcher.startListening()
-
-    // val handler = new MessageJsonHandler
-    // val reader = new StreamMessageReader(in, handler)
-    // val writer = new StreamMessageWriter(out, handler)
-    // val endpoint = new LanguageServerEndpoint(server)
-
-    // endpoint.setMessageTracer(new MessageTracer() {
-    //   override def onError(message: String, err: Throwable) {
-    //     //println(s"onError: $message - $err")
-    //     err.printStackTrace
-    //   }
-    //   override def onRead(msg: Message, s: String): Unit = {
-    //     //println(s"onRead: $msg")
-    //   }
-    //   override def onWrite(msg: Message, s: String): Unit = {
-    //     //println(s"onWrite: $msg")
-    //   }
-    // })
-    // reader.setOnError(new Consumer[Throwable] {
-    //   def accept(err: Throwable) = {
-    //     //println(s"Reader: ${err.getMessage()}")
-    //     throw err
-    //   }
-    // })
-    // writer.setOnError(new Consumer[Throwable] {
-    //   def accept(err: Throwable) = {
-    //     //println(s"Writer: ${err.getMessage()}")
-    //     throw err
-    //   }
-    // })
-    // endpoint.connect(reader, writer)
+    System.setOut(System.err)
+    scala.Console.withOut(scala.Console.err) {
+      println("hi")
+      // val launcher = LSPLauncher.createServerLauncher(server, in, out, false, new PrintWriter(System.err, true))
+      val launcher = LSPLauncher.createServerLauncher(server, in, out)
+      val client = launcher.getRemoteProxy()
+      server.connect(client)
+      launcher.startListening()
+    }
   }
 }
