414fbc616edcb70312110b643ed6e28d5d4ff9eb
Guillaume Martres <smarter@ubuntu.com>
2017-05-03 18:36:40 +0200

    Add scalaVersion to IDEConfig

--- language-server/src/dotty/tools/dotc/interactive/IDEConfig.java
+++ language-server/src/dotty/tools/dotc/interactive/IDEConfig.java
@@ -1,11 +1,10 @@
 package com.felixmulder.dotty.plugin;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-
 import java.io.File;
 
 public class IDEConfig {
   public String id;
+  public String scalaVersion;
   public File[] sources;
   public String[] scalacArgs;
   public File[] depCp;
@@ -15,8 +14,9 @@ public class IDEConfig {
   public IDEConfig() {
   }
 
-  public IDEConfig(String id, File[] sources, String[] scalacArgs, File[] depCp, File target) {
+  public IDEConfig(String id, String scalaVersion, File[] sources, String[] scalacArgs, File[] depCp, File target) {
      this.id = id;
+     this.scalaVersion = scalaVersion;
      this.sources = sources;
      this.scalacArgs = scalacArgs;
      this.depCp = depCp;
