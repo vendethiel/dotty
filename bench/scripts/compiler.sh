bb45a730bdf501f8082d05c41ba7be99dfc168ae
Guillaume Martres <smarter@ubuntu.com>
2017-10-03 23:03:32 +0200

    Compile backend too

--- bench/scripts/compiler.sh
+++ bench/scripts/compiler.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-find compiler/src/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench-bootstrapped/jmh:run 5 10" {} + | sbt
+find -L compiler/target/scala-2.12/src_managed/ compiler/src/ -type f \( -name "*.scala" -or -name "*.java" \) -exec echo "dotty-bench-bootstrapped/jmh:run 5 10" {} + | sbt
