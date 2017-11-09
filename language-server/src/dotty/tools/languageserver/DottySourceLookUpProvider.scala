7bf04e626e799300d0baf330b48dda25c6b85519
Guillaume Martres <smarter@ubuntu.com>
2017-11-09 16:07:55 +0100

    getSourceFileURI working

--- language-server/src/dotty/tools/languageserver/DottySourceLookUpProvider.scala
+++ language-server/src/dotty/tools/languageserver/DottySourceLookUpProvider.scala
@@ -5,6 +5,8 @@ import com.microsoft.java.debug.core._
 import com.microsoft.java.debug.core.adapter._
 
 import java.net.URI
+import java.nio.file._
+import java.nio.charset.StandardCharsets
 
 import java.util.logging.Level
 import java.util.logging.Logger
@@ -19,7 +21,8 @@ class DottySourceLookUpProvider(languageServer: DottyLanguageServer) extends ISo
     languageServer.synchronized {
       val uri = new URI(uriString)
       val driver = languageServer.debugDriverFor(uri)
-      val diags = driver.run(uri, languageServer.sources(uri))
+      val sourceCode = getSourceContents(uriString)
+      val diags = driver.run(uri, sourceCode)
       println("diags: " + diags)
 
       implicit val ctx = driver.currentCtx
@@ -30,42 +33,49 @@ class DottySourceLookUpProvider(languageServer: DottyLanguageServer) extends ISo
         else
           optionalColumns
 
-      System.err.println("lines: " + lines.toList)
-      System.err.println("columns: " + columns.toList)
       (lines, columns).zipped.map { (line, column) =>
         val pos = DottyLanguageServer.sourcePosition(driver, uri, line - 1, column)
-        val sym = ctx.atPhase(ctx.typerPhase) { implicit ctx =>
-          Interactive.enclosingSourceSymbol(driver.openedTrees(uri), pos)
+        val trees = driver.openedTrees(uri)
+        val defSym = ctx.atPhase(ctx.typerPhase) { implicit ctx =>
+          val path = Interactive.pathTo(trees, pos)
+          Interactive.enclosingDefinitionInPath(path).symbol
+          // Interactive.enclosingSourceSymbol(driver.openedTrees(uri), pos)
         }
-        System.err.println("pos: " + pos)
-        System.err.println("sym: " + sym)
-        // val path = ctx.atPhase(ctx.typerPhase) { implicit ctx =>
-        //   println("path: " + Interactive.pathTo(driver.openedTrees(uri), pos).map(_.show).mkString("##\n"))
-        // }
-        if (sym == NoSymbol) ""
+        println("sym: " + defSym)
+        if (defSym == NoSymbol) ""
         else {
           // TODO: Confirm that this is a robust way to get the name of the enclosing emitted class
-          val meth = ctx.atPhase(ctx.typerPhase) { implicit ctx =>
-            sym.enclosingMethod
-          }
+          // val cls = ctx.atPhase(ctx.typerPhase) { implicit ctx =>
+          //   sym.enclosingClass
+          // }
           ctx.atPhase(ctx.terminalPhase) { implicit ctx =>
-            System.err.println("owner1: " + meth.enclosingClass.fullName.mangledString)
-            meth.enclosingClass.fullName.mangledString
+            val c = defSym.enclosingClass.fullName.mangledString
+            println("c: " + c)
+            c
           }
         }
       }
     }
 
-  override def getSourceContents(uriString: String): String = languageServer.synchronized {
+  override def getSourceContents(uriString: String): String = {
     val uri = new URI(uriString)
 
-    languageServer.sources(uri)
+    new String(Files.readAllBytes(Paths.get(uri)), StandardCharsets.UTF_8)
   }
 
-  override def getSourceFileURI(fullyQualifiedName: String, sourcePath: String): String = languageServer.synchronized {
-    System.err.println("fqn: " + fullyQualifiedName)
-    ""
-  }
-
+  override def getSourceFileURI(fullyQualifiedName: String, relativeSourcePath: String): String =
+    languageServer.synchronized {
+      System.err.println("fqn: " + fullyQualifiedName)
+      System.err.println("rsp: " + relativeSourcePath)
+      languageServer.drivers.keys.flatMap(_.sourceDirectories).foreach { sourceDir =>
+        val path = sourceDir.toPath.resolve(relativeSourcePath)
+        if (Files.exists(path)) {
+          System.err.println("found: " + path)
+          return path.toUri.toString
+        }
+      }
+      System.err.println("not found")
+      ""
+    }
   override def supportsRealtimeBreakpointVerification(): Boolean = false
 }
