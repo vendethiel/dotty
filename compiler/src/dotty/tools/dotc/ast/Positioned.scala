de74691893b5c36614a8604a1a42114fae7019b3
Guillaume Martres <smarter@ubuntu.com>
2018-01-28 03:39:58 +0100

    Speed-up no-op withPos

--- compiler/src/dotty/tools/dotc/ast/Positioned.scala
+++ compiler/src/dotty/tools/dotc/ast/Positioned.scala
@@ -34,7 +34,9 @@ abstract class Positioned extends DotClass with Product {
    *  destructively and the item itself is returned.
    */
   def withPos(pos: Position): this.type = {
-    val newpd = (if (pos == curPos || curPos.isSynthetic) this else clone).asInstanceOf[Positioned]
+    if (pos == curPos)
+      return this
+    val newpd = (if (curPos.isSynthetic) this else clone).asInstanceOf[Positioned]
     newpd.setPos(pos)
     newpd.asInstanceOf[this.type]
   }
