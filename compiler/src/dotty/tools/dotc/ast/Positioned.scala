be63d1fe66edf0414724ee5be1663b3209ccc89a
Guillaume Martres <smarter@ubuntu.com>
2018-01-29 19:53:43 +0100

    Revert "Speed-up no-op withPos"
    This reverts commit ed54f594b73c72c1fbf5a585d9847f2edfd40576.
    
    Turns out, we do need to set the positions of children without position.
    This shouldn't be too expensive.

--- compiler/src/dotty/tools/dotc/ast/Positioned.scala
+++ compiler/src/dotty/tools/dotc/ast/Positioned.scala
@@ -34,9 +34,7 @@ abstract class Positioned extends DotClass with Product {
    *  destructively and the item itself is returned.
    */
   def withPos(pos: Position): this.type = {
-    if (pos == curPos)
-      return this
-    val newpd = (if (curPos.isSynthetic) this else clone).asInstanceOf[Positioned]
+    val newpd = (if (pos == curPos || curPos.isSynthetic) this else clone).asInstanceOf[Positioned]
     newpd.setPos(pos)
     newpd.asInstanceOf[this.type]
   }
